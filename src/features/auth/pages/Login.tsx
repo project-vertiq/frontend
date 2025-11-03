6ca9e1d7b572bc3d634c6bafe1bf9e9315ccb96d
prudhvialeti1611 <prudhvialeti1611@gmail.com>
2025-11-03 16:44:28 +0530

    width fix

--- src/features/auth/pages/Login.tsx
+++ src/features/auth/pages/Login.tsx
@@ -3,7 +3,7 @@ import LoginForm from "../components/LoginForm";
 
 export default function LoginPage() {
   return (
-    <div className="grid min-h-svh lg:grid-cols-2">
+    <div className="grid min-h-screen lg:grid-cols-2 w-full">
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
           <a href="#" className="flex items-center gap-2 font-medium">
--- src/features/auth/pages/Signup.tsx
+++ src/features/auth/pages/Signup.tsx
@@ -3,7 +3,7 @@ import SignupForm from "../components/SignupForm";
 
 export default function SignupPage() {
   return (
-    <div className="grid min-h-svh lg:grid-cols-2">
+    <div className="grid min-h-screen w-full lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
           <a href="#" className="flex items-center gap-2 font-medium">
